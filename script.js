6a9a70147b202c49e01ea00b4b2ee570f3cc62bd
Aleksandra Mańczak <61183229+manczakola@users.noreply.github.com>
2021-01-06 10:24:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// const canvas = document.querySelector('#canvas');
-// const ctx = canvas.getContext('2d');
-
-// ctx.fillStyle = '#ff0000';
-// ctx.fillRect(0, 0, 100, 50);
-
-// ctx.beginPath();
-// ctx.lineWidth = 3;
-// ctx.strokeStyle = '#00ff00';
-// ctx.moveTo(50, 50);
-// ctx.lineTo(100, 100);
-// ctx.lineTo(200, 150);
-// ctx.stroke();
-
 
 class Sky {
     constructor(canvas) {
